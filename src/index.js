7082189e83da5452c15b08d2b0bd7e3143a9f9fc
Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2022-09-18 11:50:03 +0600

    updated to 18 Sep 2022

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 // Import all plugins
-import * as bootstrap from 'bootstrap';
+// import * as bootstrap from 'bootstrap';
 
 import 'bootstrap'
 import '../scss/custom.scss'
+
+//import * as mdb from 'mdb-ui-kit'; // lib
+//import { Input } from 'mdb-ui-kit'; // module
+
+function showDatetime() {
+    let today = new Date();
+    let date = today.toDateString();
+    let time = today.toTimeString();
+    let dateTime = date + ' ' + time;
+    document.getElementById("clock").innerHTML = dateTime;
+    setTimeout(showDatetime, 1000);
+}
+
+showDatetime();
 
 console.log('Hello world!');
